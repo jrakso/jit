0ec2cdf9c3b875325a40188dcc412e721d70aa4b
Oskar Jonsson <oskar.jonsson17@outlook.com>
2025-07-13 20:53:31 +0200

    Started working on jit init command

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 
-int main() {
-    printf("Welcome to my version control program");
+
+int handle_init_command() {
+    if (mkdir(".jit", 0777) == -1) {
+        printf("Error creating repo!\n");
+        return 1;
+    } else {
+        printf("jit repo created successfully!\n");
+        return 0;
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    printf("Welcome to my version control program\n");
+    if (argc > 1 && strcmp(argv[1], "init") == 0) {
+        printf("you typed %s\n", argv[1]);
+        return handle_init_command();
+    }
     return 0;
 }
